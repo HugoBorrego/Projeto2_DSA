e4e673b2fe5fd167fe8187af26179e9816b2c20f
Hugo <hugo.borrego02@gmail.com>
2025-04-17 10:36:48 -0300

    Update Projeto2.py

--- Projeto2.py
+++ Projeto2.py
@@ -73,3 +73,58 @@ print(df_p6.head())
 df['Desconto'] = np.where(df['Valor_Venda'] > 1000, 0.15, 0.10)
 print(df.head())
 print(df['Desconto'].value_counts())
+
+# P8 - Considere Que a Empresa Decida Conceder o Desconto de 15% do Item Anterior. Qual Seria a Média do Valor de Venda Antes e Depois do Desconto?
+df['Valor_Venda_Desconto'] = df['Valor_Venda'] - (df['Valor_Venda'] * df['Desconto'])
+df_p8_vendas_antes_desconto = df.loc[df['Desconto'] == 0.15, 'Valor_Venda']
+df_p8_vendas_depois_desconto = df.loc[df['Desconto'] == 0.15, 'Valor_Venda_Desconto']
+media_vendas_antes_desconto = df_p8_vendas_antes_desconto.mean()
+media_vendas_depois_desconto = df_p8_vendas_depois_desconto.mean()
+print("Média das vendas antes do desconto de 15%:", round(media_vendas_antes_desconto, 2))
+print("Média das vendas depois do desconto de 15%:", round(media_vendas_depois_desconto, 2))
+
+# P9 - Qual o Média de Vendas Por Segmento, Por Ano e Por Mês?
+df['Mes'] = df['Data_Pedido'].dt.month
+df_p9 = df.groupby(['Ano', 'Mes', 'Segmento'])['Valor_Venda'].agg([np.sum, np.mean, np.median])
+anos = df_p9.index.get_level_values(0)
+meses = df_p9.index.get_level_values(1)
+segmentos = df_p9.index.get_level_values(2)
+plt.figure(figsize=(12, 6))
+sns.set()
+fig1 = sns.relplot(kind='line',
+                   data=df_p9,
+                   y='mean',
+                   x=meses,
+                   hue=segmentos,
+                   col=anos,
+                   col_wrap=4)
+plt.show()
+
+# P10 - Qual o Total de Vendas Por Categoria e SubCategoria, Considerando Somente as Top 12 SubCategorias?
+df_p10 = df.groupby(['Categoria', 'SubCategoria']).sum(numeric_only=True).sort_values('Valor_Venda', ascending=False).head(12)
+df_p10 = df_p10[['Valor_Venda']].astype(int).sort_values(by='Categoria').reset_index()
+df_p10_cat = df_p10.groupby('Categoria').sum(numeric_only=True).reset_index()
+cores_categorias = ['#5d00de', '#0ee84f', '#e80e27']
+cores_subcategorias = ['#aa8cd4', '#aa8cd5', '#aa8cd6', '#aa8cd7', '#26c957', '#26c958', '#26c959', '#26c960', '#e65e65', '#e65e66', '#e65e67', '#e65e68']
+fig, ax = plt.subplots(figsize=(18, 12))
+p1 = ax.pie(df_p10_cat['Valor_Venda'],
+            radius=1,
+            labels = df_p10_cat['Categoria'],
+            wedgeprops=dict(edgecolor='white'),
+            colors=cores_categorias)
+p2 = ax.pie(df_p10['Valor_Venda'],
+            radius=0.9,
+            labels=df_p10['SubCategoria'],
+            autopct=autopct_format(df_p10['Valor_Venda']),
+            colors=cores_subcategorias,
+            labeldistance=0.7,
+            wedgeprops=dict(edgecolor='white'),
+            pctdistance=0.53,
+            rotatelabels=True)
+centre_circle = plt.Circle((0, 0), 0.6, fc='white')
+fig = plt.gcf()
+fig.gca().add_artist(centre_circle)
+plt.annotate(text='Total de Vendas: ' + '$ ' + str(int(sum(df_p10['Valor_Venda']))), xy=(-0.2, 0))
+plt.title('Total de Vendas Por Categoria e Top 12 SubCategorias')
+plt.show()
+
